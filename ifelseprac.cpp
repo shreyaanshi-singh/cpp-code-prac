2c14a1b1667223ed8252ef46848afee5d0de6578
shreyaanshi-singh <167690914+shreyaanshi-singh@users.noreply.github.com>
2024-05-03 12:59:07 +0530

    Create ifelseprac.cpp

---
+++ ifelseprac.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n;
+    cin >> n;
+    if ( n < 7){
+    cout << "true";
+    }
+    else if ( n > 7){
+    cout << "false";
+    }
+    else {
+    cout << "i dont know";
+    }
+    std::cout << "Try programiz.pro";
+
+    return 0;
+}
